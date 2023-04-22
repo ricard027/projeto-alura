170e376270f0b95b02002e040047f43104b33673
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-04-22 06:38:01 -0300

    feat: add components file

---
+++ js/components.js
@@ -0,0 +1,8 @@
+export const AreaTextMessage = document.querySelector(".text-message-area");
+export const Inputmessage = document.querySelector("#input-message");
+export const ButtonCopyMessage = document.querySelector("#btn-copy-message");
+export const EncryptButton = document.querySelector("#encrypt");
+export const DecryptButton = document.querySelector("#decrypt");
+export const ContainerMessage = document.querySelector("#messages-container");
+export const ContainerNotAreMessage =
+  document.querySelector(".are-not-messages");
